9b2735e32ddac8563069dfa0175dbd443660ac84
pradal <christophe.pradal@inria.fr>
2020-03-27 17:54:05 +0100

    Add a central setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os, sys
+import glob
+
+packages = glob.glob('*wheat')
+
+def sh(cmd):
+    return os.system(cmd)
+
+def apply(*args):
+    status = 0
+    cmd = 'python setup.py %s'%(' '.join(*args))
+    for pkg in packages:
+        os.chdir(pkg)
+        st = sh(cmd)
+        status = max(st, status)
+        os.chdir('..')
+    return status
+if __name__ == "__main__": 
+    #print('packages', packages)
+    #print('Args', sys.argv[1:])
+    status = apply(sys.argv[1:])
+    
